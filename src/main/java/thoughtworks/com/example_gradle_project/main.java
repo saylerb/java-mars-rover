30afef564793aa1cf09c2bdcb1d6eead9d113de5
Brian Sayler <sayler.b@gmail.com>
2017-03-20 12:40:56 -0600

    initial commit

---
+++ src/main/java/thoughtworks/com/example_gradle_project/main.java
@@ -0,0 +1,11 @@
+package thoughtworks.com.example_gradle_project;
+
+/**
+ * Created by saylerb on 3/20/17.
+ */
+public class main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/thoughtworks/com/example_gradle_project/mainTest.java
@@ -0,0 +1,16 @@
+package thoughtworks.com.example_gradle_project;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by saylerb on 3/20/17.
+ */
+public class mainTest {
+
+    @Test
+    public void name() throws Exception {
+
+    }
+}
